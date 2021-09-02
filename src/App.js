78af543fd6e62208ec222d7c77e03e44c0ad1d7e
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-09-02 21:41:17 +0300

    added all parts in one

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 // import "./App.css";
 
-// 1я часть ДЗ
 import user from './components/Profile/user.json';
 import Profile from './components/Profile/Profile';
 
-// 2я часть ДЗ
-// import statisticalData from './components/Statistics/statistical-data.json';
-// import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
+import Statistics from './components/Statistics/Statistics';
 
-// 3я часть ДЗ
-// import friends from './components/FriendList/friends.json';
-// import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
+import FriendList from './components/FriendList/FriendList';
 
-// 4я часть ДЗ
-// import TransactionHistory from './components/Transactions/TransactionHistory';
-// import transactions from './components/Transactions/transactions.json';
+import TransactionHistory from './components/Transactions/TransactionHistory';
+import transactions from './components/Transactions/transactions.json';
 
 function App() {
   return (
-    // 1я часть ДЗ
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-
-    // 2я часть ДЗ
-    // <Statistics title="Upload stats" stats={statisticalData} />
-
-    // 3я часть ДЗ
-    // <FriendList friends={friends} />
-
-    // 4я часть ДЗ
-    // <TransactionHistory items={transactions} />
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 }
 
